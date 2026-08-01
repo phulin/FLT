d1e9591a5e345af921e2109bcb95f759202fb4a1
Patrick Hulin <patrick.hulin@gmail.com>
2026-08-01 21:10:38 +0000

    construct absolute irreducibility from odd element

--- FLT/Deformations/RepresentationTheory/Irreducible.lean
+++ FLT/Deformations/RepresentationTheory/Irreducible.lean
@@ -7,6 +7,7 @@ module
 
 public import Mathlib.RepresentationTheory.Irreducible
 public import FLT.Mathlib.RepresentationTheory.Basic
+public import FLT.Slop.RepresentationTheory.OddAbsIrredSlop
 
 /-!
 # Absolutely irreducible representations
@@ -26,12 +27,22 @@ variable {G : Type*} [Group G]
 
 variable {k : Type*} [Field k]
 
-variable {W : Type*} [AddCommMonoid W] [Module k W]
+variable {W : Type*} [AddCommGroup W] [Module k W]
 
 /-- `IsAbsolutelyIrreducible ρ` states that a given Representation `ρ` over a field `k`
 is absolutely irreducible, meaning that all the possible base change extensions are irreducible. -/
 class IsAbsolutelyIrreducible (ρ : Representation k G W) : Prop where
   absolutelyIrreducible :
     ∀ k' : Type u, ∀ _ : Field k', ∀ _ : Algebra k k', IsIrreducible (k' ⊗ᵣ' ρ)
+
+/-- An irreducible finite-dimensional representation is absolutely irreducible if one group
+element has a one-dimensional fixed space. -/
+theorem IsAbsolutelyIrreducible.of_finrank_eigenspace_eq_one
+    (ρ : Representation k G W) [FiniteDimensional k W] (hρ : ρ.IsIrreducible)
+    {g : G} (hg : Module.finrank k (Module.End.eigenspace (ρ g) 1) = 1) :
+    ρ.IsAbsolutelyIrreducible where
+  absolutelyIrreducible k' _ _ := by
+    change (Slop.OddRep.baseChange k' ρ).IsIrreducible
+    exact Slop.OddRep.isIrreducible_baseChange_of_finrank_eigenspace_eq_one ρ k' hρ hg
 
 end Representation
