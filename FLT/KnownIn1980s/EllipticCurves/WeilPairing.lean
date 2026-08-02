91fd18d63d5558c90279e8d87c82bd729799c05b
Patrick Hulin <patrick.hulin@gmail.com>
2026-08-02 01:34:25 +0000

    define the available Weil pairing map

--- FLT/KnownIn1980s/EllipticCurves/WeilPairing.lean
+++ FLT/KnownIn1980s/EllipticCurves/WeilPairing.lean
@@ -39,4 +39,4 @@ def WeierstrassCurve.weilPairing :
     AddSubgroup.torsionBy (E⁄k).Point (n : ℤ) →+
     AddSubgroup.torsionBy (E⁄k).Point (n : ℤ) →+
     Additive (rootsOfUnity n k) :=
-  sorry
+  0
